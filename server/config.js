91d18864017584ee6aa1b4e60773e156a7b6ee3a
zaman <Rahman.Ahkamuz@gmail.com>
2022-09-18 14:41:12 -0400

    added sql string

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    databaseURL: process.env.DATABASE_URL || "postgres://ccoenraets@localhost/university"
+    databaseURL: process.env.DATABASE_URL || "postgres://yxbsjpehxgifcn:33b116bb270ddf8a4a0e1ac39bcc1cb52d7bc86a236fd465165effac87b66aad@ec2-18-208-55-135.compute-1.amazonaws.com:5432/dblqsklr6i0dik"
 
 };
